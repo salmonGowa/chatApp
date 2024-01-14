d30a6b25645b41ae535c0ded0b684826b94858a5
salmonGowa <gowasalmon@yahoo.com>
2024-01-14 11:51:28 +0300

    chatlayer

--- chatApp/chatApp/settings.py
+++ chatApp/chatApp/settings.py
@@ -71,7 +71,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'chatApp.wsgi.application'
 ASGI_APPLICATION="chatApp.routing.application"
-
+CHANNEL_LAYER={
+    "default":{
+        "BACKEND":"channels_redis.core.RedisChannelLayer",
+        "CONFIG":{
+            "hosts":[("127.0.0.1", 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
