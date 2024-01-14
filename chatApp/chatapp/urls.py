17283e7ef0a340998c05700931724b48d7d257d7
salmonGowa <gowasalmon@yahoo.com>
2024-01-14 11:16:09 +0300

    routing

--- chatApp/chatapp/urls.py
+++ chatApp/chatapp/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 
 
 
-from .views import index
+from . import views
 
 
 app_name='chatapp'
 
 urlpatterns = [
-    path('',index,name="index"),
+    path('',views.index,name="index"),
+    path("<str:room_name>/", views.room, name="room"),
 ]
--- chatApp/chatapp/views.py
+++ chatApp/chatapp/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'chat/index.html',{})
+    return render(request,'chatapp/index.html')
 
-def room (request):
-    return render(request,'chat/room.html',{
-        'room_name_json':mark_safe(json.dumps(room_name))
-    })
+def room (request,room_name):
+    return render(request,'chatapp/room.html',{"room_name":room_name})
