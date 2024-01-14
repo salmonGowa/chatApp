911ce57d0b9d30ea6b0a8cfa9a8578f5b5dc31fa
salmonGowa <gowasalmon@yahoo.com>
2024-01-14 10:44:28 +0300

    consumer.py

--- chatApp/chatapp/views.py
+++ chatApp/chatapp/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'chat/index.html',{})
+
+def room (request):
+    return render(request,'chat/room.html',{
+        'room_name_json':mark_safe(json.dumps(room_name))
+    })
